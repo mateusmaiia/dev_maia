fd2115ac33fe86c30fa6032289e8383f81375d72
maia <matteusmaia889@gmail.com>
2024-05-08 18:06:15 -0300

    update: change gap social media and toggletheme container

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -40,7 +40,7 @@ export function Header() {
             </li> */}
           </ul>
         </nav>
-        <div className='flex items-center gap-2'>
+        <div className='flex items-center gap-1'>
           <SocialMedias />
           <SocialMedias />
           <ThemeToggle />
