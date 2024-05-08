ee7473e89b50f851a94150e953b553fe6eb30774
maia <matteusmaia889@gmail.com>
2024-05-08 17:53:59 -0300

    faet: addind margin left

--- src/components/theme/theme-toggle.tsx
+++ src/components/theme/theme-toggle.tsx
@@ -13,9 +13,9 @@ export function ThemeToggle() {
   const { setTheme } = useTheme()
 
   return (
-    <DropdownMenu>
+    <DropdownMenu >
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="border-0 bg-transparent">
+        <Button variant="outline" size="icon" className="border-0 bg-transparent ml-1">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
