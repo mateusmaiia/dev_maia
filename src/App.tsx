caa5eb64f1828d1dad5ac7c41ee25abf21c3ada7
Maia <matteusmaia889@gmail.com>
2024-05-08 12:56:29 -0300

    feat: comps importeds

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-
+import { About } from './components/about'
+import { Header } from './components/header'
+import { Section } from './components/section'
+import { ThemeProvider } from './components/theme/themeProvider'
+import './globals.css'
+import { Projects } from './components/projects'
 
 export function App() {
 
   return (
-    <>
-     <h1>Hello Maia</h1>
-    </>
+    <ThemeProvider  storageKey='dev-maia-theme' defaultTheme='dark'>
+    <div className="flex min-h-screen flex-col antialiased ">
+      <Header />
+      <Section />
+      <About />
+      <Projects />
+    </div>
+    </ThemeProvider>
   )
 }
 
