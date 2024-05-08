577df9b340f226a67f2465afa718ef81533d9d0d
maia <matteusmaia889@gmail.com>
2024-05-08 17:52:50 -0300

    update: exchanging hoverContent by shadcn ui and replacing it with Tooltip

--- src/components/projectsCard.tsx
+++ src/components/projectsCard.tsx
@@ -1,20 +1,29 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
-import { HoverCardTrigger, HoverCardContent, HoverCard } from "./ui/hover-card";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
 
 export function ProjectsCard(){
   return(
-    <Card className="cursor-pointer">
-      <HoverCard>
-        <HoverCardTrigger>
-          <CardContent className="pt-6">
-            <img className="h-48  w-fit" src="https://raw.githubusercontent.com/famgz/famgz/main/presentations/food-ordering-app/0.jpg" />
-          </CardContent>
-        </HoverCardTrigger>
-        <HoverCardContent>
-          Clique pare ver mais sobre o projeto.
-        </HoverCardContent>      
-      </HoverCard>
-      <CardHeader className="space-y-0">
+    <Card className="cursor-pointer dark:hover:bg-white/5 hover:bg-black/10 hover:scale-105 transition ease-in-out delay-75">
+      <CardContent className='p-6'>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+                <img className="h-48  w-fit" src="https://raw.githubusercontent.com/famgz/famgz/main/presentations/food-ordering-app/0.jpg" />
+            </TooltipTrigger>
+            <TooltipContent className='bg-background p-4 font-semibold text-lg'>
+              Clique pare ver mais sobre o projeto.
+            </TooltipContent>      
+          </Tooltip>
+        </TooltipProvider>
+      </CardContent>
+      <CardHeader className="space-y-0 p-6">
         <CardTitle className="text-start">Project Name</CardTitle>
         <CardDescription className="text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem distinctio consequatur atque possimus magni quo libero ratione eo.</CardDescription>
       </CardHeader>
