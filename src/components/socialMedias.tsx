d8aecaa0f6774a1b3b4d9bc6c66fdaeff58e8575
maia <matteusmaia889@gmail.com>
2024-05-08 18:09:02 -0300

    feat: social media flex-1

--- src/components/socialMedias.tsx
+++ src/components/socialMedias.tsx
@@ -13,8 +13,8 @@ export function SocialMedias(){
         <TooltipProvider>
             <Tooltip>
                 <TooltipTrigger asChild>
-                    <Button variant="ghost" size="icon" className='p-4  w-10 h-10  hover:border-b-2 rounded-none  border-b-cyan-600 '>
-                        <a href="https://github.com/mateusmaiia" target='_blank' >
+                    <Button  variant="ghost" size="icon" className='p-4  w-10 h-10  hover:border-b-2 rounded-none  border-b-cyan-600 '>
+                        <a href="https://github.com/mateusmaiia" target='_blank' className='flex flex-1'>
                             <Github size={18}/>
                         </a>
                     </Button>
