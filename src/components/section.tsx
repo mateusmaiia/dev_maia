aebc4f0d7868959ced13873bd18285cf35157900
maia <matteusmaia889@gmail.com>
2024-05-08 17:53:15 -0300

    update: margin top

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -5,9 +5,11 @@ export function Section() {
   return (
     <div className="w-full max-w-screen-lg mx-auto mt-24  md:mt-44 grid md:grid-cols-2 grid-row gap-4 px-8 md:h-full">
       <div className="order-last md:order-first w-full mx-auto flex items-center  flex-col md:items-start justify-center">
+        
         <h1 className="text-4xl md:text-5xl tracking-tighter font-semibold">Mateus Maia</h1>
-        <h3 className="text-accent-foreground-foreground mt-1 tracking-wide ">Desenvolvedor de software</h3>
-        <div className="flex flex-col md:flex-row md:gap-5 mt-5">
+        <h3 className="text-accent-foreground-foreground mt-1 tracking-wide text-zinc-300 ">Desenvolvedor de software</h3>
+          
+        <div className="flex flex-col md:flex-row md:gap-5 mt-6">
           <Button className="w-full md:w-auto text-sm md:text-base bg-[#172554] text-white dark:hover:text-black flex items-center justify-center gap-2" size="lg">
             <Download size="22px" /> Baixar CV
           </Button>
