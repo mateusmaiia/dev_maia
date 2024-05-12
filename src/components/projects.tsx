487087e239b503ad310cad91f828ce4e8235affa
Maia <matteusmaia889@gmail.com>
2024-05-11 22:29:19 -0300

    faet: using .map for show the projects

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -3,9 +3,32 @@ import { ProjectCardTrigger } from "./projectCardTrigger"
 
 export function Projects ( ){
 
-  const images = [
-    "https://raw.githubusercontent.com/famgz/famgz/main/presentations/controle-de-caixas/1.jpg",
-    "https://raw.githubusercontent.com/famgz/famgz/main/presentations/barbershop-app/1.jpg",
+  const projects = [
+    {
+      urlCardTrigger: "https://raw.githubusercontent.com/famgz/famgz/main/presentations/barbershop-app/1.jpg",
+      ProjectCardDescriptionTrigger: "Project Card Description 1",
+      ProjectNameDialog: "Projec Name Dialog 1",
+      ProjectCardNameTrigger: "Project Card Name Trigger",
+      ProjectDescriptionDialog: "Project DescriptionDialog",
+      CarouselImages: [
+        "https://raw.githubusercontent.com/famgz/famgz/main/presentations/barbershop-app/1.jpg",
+        "https://raw.githubusercontent.com/famgz/famgz/main/presentations/controle-de-caixas/1.jpg",
+        "https://raw.githubusercontent.com/famgz/famgz/main/presentations/barbershop-app/1.jpg",
+      ]
+    },
+    {
+      urlCardTrigger:"https://raw.githubusercontent.com/famgz/famgz/main/presentations/controle-de-caixas/1.jpg",
+      
+      ProjectCardDescriptionTrigger: "Project Card Description 1",
+      ProjectNameDialog: "Projec Name Dialog 1",
+      ProjectCardNameTrigger: "Project Card Name Trigger",
+      ProjectDescriptionDialog: "Project DescriptionDialog",
+      CarouselImages: [
+        "https://raw.githubusercontent.com/famgz/famgz/main/presentations/controle-de-caixas/1.jpg",
+        "https://raw.githubusercontent.com/famgz/famgz/main/presentations/barbershop-app/1.jpg",
+
+      ]
+    },
   ]
 
   return(
@@ -13,22 +36,17 @@ export function Projects ( ){
         <h3 className="text-center text-4xl font-semibold mb-6">Projetos</h3>
         <div className="grid grid-cols-1 md:grid-cols-3 w-full max-w-5xl gap-6">
        
-          <ProjectCardTrigger 
-            urlCardTrigger="https://raw.githubusercontent.com/famgz/famgz/main/presentations/barbershop-app/1.jpg"
-            ProjectCardDescriptionTrigger="Project Card Description 1"
-            ProjectNameDialog="Projec Name Dialog 1"
-            ProjectCardNameTrigger="Project Card Name Trigger"
-            ProjectDescriptionDialog="Project DescriptionDialog"
-            CarouselImages={images}
-          />
-          <ProjectCardTrigger 
-            urlCardTrigger="https://raw.githubusercontent.com/famgz/famgz/main/presentations/food-ordering-app/0.jpg" 
-            ProjectCardDescriptionTrigger="Project Card Description 2"
-            ProjectNameDialog="Projec Name Dialog 2"
-            ProjectCardNameTrigger="Project Card Name Trigger"
-            ProjectDescriptionDialog="Project DescriptionDialog"
-            CarouselImages={images}
-          />
+         {projects.map((project, index) => (
+            <ProjectCardTrigger 
+              key={index}
+              CarouselImages={project.CarouselImages}
+              ProjectCardDescriptionTrigger={project.ProjectCardDescriptionTrigger}
+              ProjectCardNameTrigger={project.ProjectCardNameTrigger}
+              ProjectDescriptionDialog={project.ProjectDescriptionDialog}
+              ProjectNameDialog={project.ProjectNameDialog}
+              urlCardTrigger={project.urlCardTrigger}
+            />
+         ))}
         </div>
     </div>
   )
