f7f50ab415192edf81a1fb9419836dc451e1b6b9
Maia <matteusmaia889@gmail.com>
2024-05-11 22:13:05 -0300

    feat: ProjectCardTriggers props

--- src/components/projectCardTrigger.tsx
+++ src/components/projectCardTrigger.tsx
@@ -6,21 +6,29 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import { ProjectsCard } from "./projectsCard"
 import { CarouselDApiDemo } from "./carrousel"
+import { ProjectsCard } from "./projectsCard"
 
-export function ProjectCardTrigger(){
+export interface ProjectCardTriggerProps{
+  ProjectCardDescriptionTrigger: string ,
+  ProjectCardNameTrigger: string,
+  ProjectNameDialog: string,
+  ProjectDescriptionDialog: string,
+  urlCardTrigger: string,
+  CarouselImages?: string[]
+}
+export function ProjectCardTrigger({ProjectCardDescriptionTrigger, ProjectCardNameTrigger, ProjectDescriptionDialog, ProjectNameDialog, urlCardTrigger, CarouselImages }: ProjectCardTriggerProps){
   return(
     <Dialog >
       <DialogTrigger >
-        <ProjectsCard />
+        <ProjectsCard ProjectCardDescription={ProjectCardDescriptionTrigger} ProjectCardName={ProjectCardNameTrigger} urlCard={urlCardTrigger}/>
       </DialogTrigger>
       <DialogContent className="h-3/4">
         <DialogHeader className="flex items-center">
-          <DialogTitle className="text-center py-4 pb-8 text-3xl">Project Name</DialogTitle>
-          <CarouselDApiDemo />
+          <DialogTitle className="text-center py-4 pb-8 text-3xl">{ProjectNameDialog}</DialogTitle>
+          <CarouselDApiDemo CarouselImages={CarouselImages}/>
           <DialogDescription className="text-lg mt-2">
-            Aplicação fullstack com interface de admin para pedidos online de pizzaria.
+            {ProjectDescriptionDialog}
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
