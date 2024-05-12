9375a79358d4833cbc3ac6c74ad54b3cfd73b669
Maia <matteusmaia889@gmail.com>
2024-05-12 18:38:46 -0300

    feat: skillsCard component

---
+++ src/components/skillsCard.tsx
@@ -0,0 +1,7 @@
+export function SkillCard(){
+  return(
+    <div className="w-44 h-44 bg-slate-500/10 hover:bg-white/10 border hover:border-b-gray-800 flex items-center justify-center cursor-pointer" >
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" className="w-24 h-24" alt="" />
+    </div>
+  )
+}
