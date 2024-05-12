e4b7a4ef0b0773060755a75937417e80203a1ff9
Maia <matteusmaia889@gmail.com>
2024-05-11 22:13:43 -0300

    feat: adding props

--- src/components/projectsCard.tsx
+++ src/components/projectsCard.tsx
@@ -7,15 +7,20 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip"
 
+export interface ProjectCardProps{
+  ProjectCardName: string,
+  ProjectCardDescription: string,
+  urlCard: string
+}
 
-export function ProjectsCard(){
+export function ProjectsCard({ProjectCardName, ProjectCardDescription, urlCard}: ProjectCardProps){
   return(
     <Card className="cursor-pointer dark:hover:bg-white/5 hover:bg-black/10 hover:scale-105 transition ease-in-out delay-75">
       <CardContent className='p-6'>
         <TooltipProvider>
           <Tooltip>
             <TooltipTrigger>
-                <img className="h-48  w-fit" src="https://raw.githubusercontent.com/famgz/famgz/main/presentations/food-ordering-app/0.jpg" />
+                <img className="h-48  w-fit" src={urlCard} />
             </TooltipTrigger>
             <TooltipContent className='bg-background p-4 font-semibold text-lg'>
               Clique pare ver mais sobre o projeto.
@@ -24,8 +29,8 @@ export function ProjectsCard(){
         </TooltipProvider>
       </CardContent>
       <CardHeader className="space-y-0 p-6">
-        <CardTitle className="text-start">Project Name</CardTitle>
-        <CardDescription className="text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem distinctio consequatur atque possimus magni quo libero ratione eo.</CardDescription>
+        <CardTitle className="text-start">{ProjectCardName}</CardTitle>
+        <CardDescription className="text-start">{ProjectCardDescription}</CardDescription>
       </CardHeader>
     </Card>
   )
