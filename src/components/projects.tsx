5f3858d2b5e1cf417ec79b92eee91f057e998ecf
Maia <matteusmaia889@gmail.com>
2024-05-11 22:13:32 -0300

    feat: importing and adding the prop values

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -2,17 +2,33 @@ import { ProjectCardTrigger } from "./projectCardTrigger"
  
 
 export function Projects ( ){
+
+  const images = [
+    "https://raw.githubusercontent.com/famgz/famgz/main/presentations/controle-de-caixas/1.jpg",
+    "https://raw.githubusercontent.com/famgz/famgz/main/presentations/barbershop-app/1.jpg",
+  ]
+
   return(
     <div id="projetos" className="w-full flex  items-center flex-col py-24 mx-auto px-4 mb-24">
         <h3 className="text-center text-4xl font-semibold mb-6">Projetos</h3>
         <div className="grid grid-cols-1 md:grid-cols-3 w-full max-w-5xl gap-6">
        
-          <ProjectCardTrigger />
-          <ProjectCardTrigger />
-          <ProjectCardTrigger />
-          <ProjectCardTrigger />
-          <ProjectCardTrigger />
-          <ProjectCardTrigger />
+          <ProjectCardTrigger 
+            urlCardTrigger="https://raw.githubusercontent.com/famgz/famgz/main/presentations/barbershop-app/1.jpg"
+            ProjectCardDescriptionTrigger="Project Card Description 1"
+            ProjectNameDialog="Projec Name Dialog 1"
+            ProjectCardNameTrigger="Project Card Name Trigger"
+            ProjectDescriptionDialog="Project DescriptionDialog"
+            CarouselImages={images}
+          />
+          <ProjectCardTrigger 
+            urlCardTrigger="https://raw.githubusercontent.com/famgz/famgz/main/presentations/food-ordering-app/0.jpg" 
+            ProjectCardDescriptionTrigger="Project Card Description 2"
+            ProjectNameDialog="Projec Name Dialog 2"
+            ProjectCardNameTrigger="Project Card Name Trigger"
+            ProjectDescriptionDialog="Project DescriptionDialog"
+            CarouselImages={images}
+          />
         </div>
     </div>
   )
