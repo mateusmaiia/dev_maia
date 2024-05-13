a5bd7cfc64a7cbb0e234fdedb5db19e71a01c650
maia <matteusmaia889@gmail.com>
2024-05-13 15:21:18 -0300

    update: text-color

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,7 +2,7 @@ export function About(){
   return(
     <div id="about" className="w-full flex bg-card items-center flex-col justify-center border-y md:max-w-[1500px] border-y-white/10 h-4/6 py-20 mx-auto  ">
       <h2 className="text-center text-4xl font-semibold mb-6">Sobre</h2>
-      <p className="text-center text-slate-400 text-lg md:text-lg w-4/6 max-w-3xl  font-light tracking-wider">
+      <p className="text-center dark:text-slate-400 text-lg md:text-lg w-4/6 max-w-3xl  font-light tracking-wider">
         Desenvolvedor de software desde 2022 com experiência de programação em aplicações Front-end e Back-end, análise e gerenciamento de dados e APIs, scripts utilitários e de automação. Rápida capacidade de assimilação e aprendizado, com domínio de tecnologias como Javascript Typescript, React, React Native, Next.js, Tailwind, PostgreSQL, MySQL.Recentemente dedica-se à criação de aplicações Full-stack com React, Next.js, React Native e Tailwind
       </p>
     </div>
