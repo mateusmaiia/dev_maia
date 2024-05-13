e975d8f393d4907522583f1dd93603d944aced0f
Maia <matteusmaia889@gmail.com>
2024-05-13 03:52:54 -0300

    feat: added language skills logo

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,16 +1,29 @@
 import { SkillCard } from "./skillsCard";
 
+
+const skillsLogo = [
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg"
+]
 export function Skills(){
   return(
-    <div className="mx-auto w-full border border-green-500 h-[500px] p-4 px-72">
-        <h1 className="text-center text-4xl font-semibold mb-8">Skills</h1>
-        <div className="grid grid-cols-5 w-full justify-items-center gap-2">
-          <SkillCard />
-          <SkillCard />
-          <SkillCard />
-          <SkillCard />
-          <SkillCard />
-          <SkillCard />
+    <div id="skills" className="mx-auto w-full borde border-t-white/10 md:h-[760px] p-4 md:p-4 md:px-72">
+        <h1 className="text-center text-4xl font-semibold mb-8 mt-10">Skills</h1>
+        <div className="grid grid-cols-2 md:grid-cols-5 w-full  items-center justify-center md:gap-2 gap-2 gap-y-2">
+          {skillsLogo.map((skillLogo, index) => (
+            <SkillCard key={index} urlSkill={skillLogo} />
+          ))}
         </div>
     </div>
   )
