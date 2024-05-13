8fa1ddc72eeba911ac0f69eea5761170ec428284
maia <matteusmaia889@gmail.com>
2024-05-13 15:21:49 -0300

    feat: added justify-center

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -69,7 +69,7 @@ export function Projects ( ){
   ]
 
   return(
-    <div id="projetos" className="w-full flex  items-center flex-col py-24 mx-auto px-4 mb-2">
+    <div id="projetos" className="w-full flex justify-center items-center flex-col py-24 mx-auto px-4 md:mb-2">
         <h3 className="text-center text-4xl font-semibold mb-6">Projetos</h3>
         <div className="grid grid-cols-1 md:grid-cols-3 w-full max-w-5xl gap-6">
        
