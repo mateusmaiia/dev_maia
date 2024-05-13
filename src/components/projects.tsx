93a9c8f9a9283d71d563cf176cbe46f170646e27
Maia <matteusmaia889@gmail.com>
2024-05-13 03:52:14 -0300

    update: name example change

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -8,13 +8,37 @@ export function Projects ( ){
       urlCardTrigger:"https://raw.githubusercontent.com/famgz/famgz/main/presentations/controle-de-caixas/1.jpg",
       
       ProjectCardDescriptionTrigger: "Project Card Description 1",
-      ProjectNameDialog: "Projec Name Dialog 1",
-      ProjectCardNameTrigger: "Project Card Name Trigger",
+      ProjectNameDialog: "Projec Name Dialog 2",
+      ProjectCardNameTrigger: "Projecto de rola",
       ProjectDescriptionDialog: "Project DescriptionDialog",
       CarouselImages: [
         "https://raw.githubusercontent.com/famgz/famgz/main/presentations/controle-de-caixas/1.jpg",
         "https://raw.githubusercontent.com/famgz/famgz/main/presentations/barbershop-app/1.jpg",
         
+      ]
+    },
+    {
+      urlCardTrigger: "https://raw.githubusercontent.com/famgz/famgz/main/presentations/barbershop-app/1.jpg",
+      ProjectCardDescriptionTrigger: "Project Card Description 1",
+      ProjectNameDialog: "Projec Name Dialog 1",
+      ProjectCardNameTrigger: "Project Dadowka",
+      ProjectDescriptionDialog: "Project DescriptionDialog",
+      CarouselImages: [
+        "https://raw.githubusercontent.com/famgz/famgz/main/presentations/barbershop-app/1.jpg",
+        "https://raw.githubusercontent.com/famgz/famgz/main/presentations/controle-de-caixas/1.jpg",
+        "https://raw.githubusercontent.com/famgz/famgz/main/presentations/barbershop-app/1.jpg",
+      ]
+    },
+    {
+      urlCardTrigger: "https://raw.githubusercontent.com/famgz/famgz/main/presentations/barbershop-app/1.jpg",
+      ProjectCardDescriptionTrigger: "Project Card Description 1",
+      ProjectNameDialog: "Projec Name Dialog 1",
+      ProjectCardNameTrigger: "Project Card Name Trigger",
+      ProjectDescriptionDialog: "Project DescriptionDialog",
+      CarouselImages: [
+        "https://raw.githubusercontent.com/famgz/famgz/main/presentations/barbershop-app/1.jpg",
+        "https://raw.githubusercontent.com/famgz/famgz/main/presentations/controle-de-caixas/1.jpg",
+        "https://raw.githubusercontent.com/famgz/famgz/main/presentations/barbershop-app/1.jpg",
       ]
     },
     {
@@ -45,7 +69,7 @@ export function Projects ( ){
   ]
 
   return(
-    <div id="projetos" className="w-full flex  items-center flex-col py-24 mx-auto px-4 mb-24">
+    <div id="projetos" className="w-full flex  items-center flex-col py-24 mx-auto px-4 mb-2">
         <h3 className="text-center text-4xl font-semibold mb-6">Projetos</h3>
         <div className="grid grid-cols-1 md:grid-cols-3 w-full max-w-5xl gap-6">
        
