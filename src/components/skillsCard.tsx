4f6e56f64bf0b474c9714f8b9bec237880ff36e8
Maia <matteusmaia889@gmail.com>
2024-05-13 03:53:20 -0300

    feat: SkillCard types

--- src/components/skillsCard.tsx
+++ src/components/skillsCard.tsx
@@ -1,7 +1,11 @@
-export function SkillCard(){
+
+interface SkillCardProps{
+  urlSkill: string
+}
+export function SkillCard({urlSkill}: SkillCardProps){
   return(
-    <div className="w-44 h-44 bg-slate-500/10 hover:bg-white/10 border hover:border-b-gray-800 flex items-center justify-center cursor-pointer" >
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" className="w-24 h-24" alt="" />
+    <div className="md:w-44 md:h-44 w-40 h-40 bg-slate-500/10 hover:bg-white/10 border hover:border-b-gray-800 flex items-center justify-center cursor-pointer" >
+      <img src={urlSkill} className="w-24 h-24" alt="" />
     </div>
   )
 }
